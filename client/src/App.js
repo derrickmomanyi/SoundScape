fff2a90e5d588f0b39e3cd23b6cfbba31ce96e0d
Derrick Momanyi <derrick.momanyi@student.moringaschool.com>
2023-03-24 21:20:59 +0300

    Added session & user controllers, models, routes and ckient

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import { Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Login from "./components/Login";
+import Signup from "./components/Signup";
 
 function App() {
+  const [user, setUser] = useState(null);
+
+  useEffect(() => {
+    fetch('/me')
+    .then((res) => {
+      if (res.ok) {
+        res.json().then((user) => setUser(user));
+      }
+    })
+  })
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <Navbar user = {user} setUser = {setUser}/>
+    <Routes>
+      <Route path = '/signup'>
+          <Signup  setUser = {setUser}/>
+      </Route>
+      <Route>
+          <Login setUser = {setUser}/>
+      </Route>
+    </Routes>
+    </>
   );
 }
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  resources :users, only: [ :show, :create ]
+  
+  post '/login', to: 'session#create'
+  delete '/logout', to: 'session#destroy'
+  get '/me', to: 'users#show'
+
   get '/hello', to: 'application#hello_world'
 
   get '*path',
