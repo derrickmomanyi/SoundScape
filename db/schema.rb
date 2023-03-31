ae73bbda38f4e34f70785e3f9091809b0eff15cf
Derrick Momanyi <derrick.momanyi@student.moringaschool.com>
2023-03-31 13:42:08 +0300

    Changed schema order

--- db/schema.rb
+++ db/schema.rb
@@ -14,6 +14,15 @@ ActiveRecord::Schema[7.0].define(version: 2023_03_27_212619) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
+
+  create_table "artists", force: :cascade do |t|
+    t.string "name"
+    t.string "image"
+    t.string "bio"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "albums", force: :cascade do |t|
     t.string "title"
     t.integer "year"
@@ -27,35 +36,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_03_27_212619) do
     t.index ["artist_id"], name: "index_albums_on_artist_id"
   end
 
-  create_table "artists", force: :cascade do |t|
-    t.string "name"
-    t.string "image"
-    t.string "bio"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
 
-  create_table "song_video_comments", force: :cascade do |t|
-    t.integer "user_id", null: false
-    t.string "body"
-    t.integer "song_video_id", null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["song_video_id"], name: "index_song_video_comments_on_song_video_id"
-    t.index ["user_id"], name: "index_song_video_comments_on_user_id"
-  end
-
-  create_table "song_videos", force: :cascade do |t|
-    t.integer "user_id", null: false
-    t.integer "song_id", null: false
-    t.string "title"
-    t.string "video_url"
-    t.text "comments"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["song_id"], name: "index_song_videos_on_song_id"
-    t.index ["user_id"], name: "index_song_videos_on_user_id"
-  end
 
   create_table "songs", force: :cascade do |t|
     t.string "title"
@@ -73,6 +54,39 @@ ActiveRecord::Schema[7.0].define(version: 2023_03_27_212619) do
     t.index ["album_id"], name: "index_songs_on_album_id"
     t.index ["artist_id"], name: "index_songs_on_artist_id"
   end
+
+  create_table "users", force: :cascade do |t|
+    t.string "username"
+    t.string "password_digest"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "song_videos", force: :cascade do |t|
+    t.integer "user_id", null: false
+    t.integer "song_id", null: false
+    t.string "title"
+    t.string "video_url"
+    t.text "comments"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["song_id"], name: "index_song_videos_on_song_id"
+    t.index ["user_id"], name: "index_song_videos_on_user_id"
+  end
+
+
+  create_table "song_video_comments", force: :cascade do |t|
+    t.integer "user_id", null: false
+    t.string "body"
+    t.integer "song_video_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["song_video_id"], name: "index_song_video_comments_on_song_video_id"
+    t.index ["user_id"], name: "index_song_video_comments_on_user_id"
+  end
+
+ 
+
 
   create_table "user_albums", force: :cascade do |t|
     t.integer "user_id", null: false
@@ -101,12 +115,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_03_27_212619) do
     t.index ["user_id"], name: "index_user_songs_on_user_id"
   end
 
-  create_table "users", force: :cascade do |t|
-    t.string "username"
-    t.string "password_digest"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
+  
 
   add_foreign_key "albums", "artists"
   add_foreign_key "song_video_comments", "song_videos"
